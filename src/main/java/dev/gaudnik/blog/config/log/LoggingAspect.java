b69d5a1f1a6108c67b56b23592182a5aeba6112f
bq666 <wojciech.gaudnik@gmail.com>
2021-07-24 08:33:09 +0200

    logging

---
+++ src/main/java/dev/gaudnik/blog/config/log/LoggingAspect.java
@@ -0,0 +1,38 @@
+package dev.gaudnik.blog.config.log;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class LoggingAspect {
+
+	@Value("${blog.logging}")
+	String logging;
+
+	@Around("@annotation(Logging)")
+	public Object log(ProceedingJoinPoint joinPoint) throws Throwable {
+		if (logging.equals("true")) {
+		ObjectMapper mapper = new ObjectMapper();
+		String methodName = joinPoint.getSignature().getName();
+		String className = joinPoint.getTarget().getClass().toString();
+		Object[] array = joinPoint.getArgs();
+		log.info("method invoked " + className + " : " + methodName + "()" + "arguments : "
+				+ mapper.writeValueAsString(array));
+		Object object = joinPoint.proceed();
+		log.info(className + " : " + methodName + "()" + "Response : "
+				+ mapper.writeValueAsString(object));
+		return object;
+		} else if (logging.equals("false")) {
+			return joinPoint.proceed();
+		}
+		throw new IllegalArgumentException("Wrong value in application.properties, blog.logging should be \"true\" or \"false\"");
+
+	}
+}
--- src/main/java/dev/gaudnik/blog/controler/BlogController.java
+++ src/main/java/dev/gaudnik/blog/controler/BlogController.java
@@ -1,11 +1,13 @@
 package dev.gaudnik.blog.controler;
 
+import dev.gaudnik.blog.config.log.Logging;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BlogController {
 
+	@Logging
 	@GetMapping(value ="/post", produces = "application/json")
 	public String getAllPosts() {
 		return "test";
